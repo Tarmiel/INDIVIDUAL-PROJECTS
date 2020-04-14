be040687c8692843ce31178802ccd3d9cd8f1164
Tarmiel <tarmiellul@gmail.com>
2020-04-14 14:58:09 -0300

    a

--- dinamic_ListaTarefas/app_lista_tarefas/tarefa_controller.php
+++ dinamic_ListaTarefas/app_lista_tarefas/tarefa_controller.php
@@ -33,6 +33,24 @@
      if($tarefaService->atualizar()){
        header('Location:todas_tarefas.php');
      }
+    }else if($acao == 'remover'){
+      $tarefa = new Tarefa();
+      $tarefa->__set('id',$_GET['id']);
+
+      $conexao = new Conexao();
+      $tarefaService = new TarefaService($conexao,$tarefa);
+      $tarefaService->remover();
+      header('Location:todas_tarefas.php');
+    }else if($acao == 'marcarRealizada'){
+      $tarefa = new Tarefa();
+      $tarefa->__set('id',$_GET['id'])->__set('id_status',2);
+      $conexao = new Conexao();
+      $tarefaService = new TarefaService($conexao,$tarefa);
+      $tarefaService->marcarRealizada();
+      header('Location:todas_tarefas.php');
+
+
     }
+    
 
  ?>
