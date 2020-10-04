7dbe0caccca959288aa53bfd21483a043c63ac27
Tarmiel <tarmiellul@gmail.com>
2020-10-04 16:22:30 -0300

    aaa

--- II.Dinamic/8__Django_API-REST.Teste/api_ToDo/app/views.py
+++ II.Dinamic/8__Django_API-REST.Teste/api_ToDo/app/views.py
@@ -3,13 +3,18 @@ from app.serializers import TodoSerializer
 
 from rest_framework import generics
 from rest_framework import status
+from rest_framework import viewsets
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.exceptions import NotFound
 
 # Create your views here.
-# metodos automaticos
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+"""
 class TodoListAndCreate(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
@@ -17,8 +22,7 @@ class TodoListAndCreate(generics.ListCreateAPIView):
 class TodoDetailChangeAndDelete(generics.RetrieveUpdateDestroyAPIView):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
-
-# Formas de criar
+"""
 
 """
 class TodoListAndCreate(APIView):
